f65974a8cbc483a1a36b135ddf7968942dfca794
zoybzo <hadequing@gmail.com>
2025-03-29 00:37:39 +0800

    update custom scripts

--- custom_scripts/inference_lora_sdxl.sh
+++ custom_scripts/inference_lora_sdxl.sh
@@ -4,11 +4,11 @@ GPU_ID=$1
 DATASET_ID=$2
 # 检查参数是否为空，并设置默认值
 if [ -z "$GPU_ID" ]; then
-    param1="6"
+    GPU_ID="6"
 fi
 
 if [ -z "$DATASET_ID" ]; then
-    param2="42"
+    DATASET_ID="42"
 fi
 
 if [ -z "$COUNTER" ]; then
@@ -30,12 +30,12 @@ echo "Current date environment variable: $DATE_VAR"
 $MHOME/Projects/kohya_ss/venv/bin/accelerate launch \
   --dynamo_backend no \
   --dynamo_mode default \
-  --gpu_ids $(GPU_ID) \
+  --gpu_ids $GPU_ID \
   --mixed_precision fp16 \
   --num_processes 1 \
   --num_machines 1 \
   --num_cpu_threads_per_process 2 \
   $MHOME/Projects/kohya_ss/sd-scripts/sdxl_train_network.py \
-  --train_data_dir "$DATASET_HOME/kafka_official_p$(DATASET_ID)" \
-  --output_name "test-$(DATE_VAR)-$(FORMATTED_COUNTER)-sdxl_base_1.0-p$(DATASET_ID)" \
+  --train_data_dir "$DATASET_HOME/kafka_official_p$DATASET_ID" \
+  --output_name "test-${DATE_VAR}-${FORMATTED_COUNTER}-sdxl_base_1.0-p$DATASET_ID" \
   --config_file config_lora_sdxl-base-1.0_ai-char-1.1.toml
