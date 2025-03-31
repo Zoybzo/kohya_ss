8cdcc788a1f23f02b780b6ef1b3207ea8b15d68d
zoybzo <hadequing@gmail.com>
2025-03-31 17:26:34 +0800

    Update counter

--- custom_scripts/inference_lora_sdxl.sh
+++ custom_scripts/inference_lora_sdxl.sh
@@ -40,5 +40,5 @@ $MHOME/Projects/kohya_ss/venv/bin/accelerate launch \
   --num_cpu_threads_per_process 2 \
   $MHOME/Projects/kohya_ss/sd-scripts/sdxl_train_network.py \
   --train_data_dir "$DATASET_HOME/kafka_official_p$DATASET_ID" \
-  --output_name "test-${DATE_VAR}-${FORMATTED_COUNTER}-sdxl_base_1.0-p$DATASET_ID" \
+  --output_name "test-${DATE_VAR}-${COUNTER}-sdxl_base_1.0-p$DATASET_ID" \
   --config_file custom_configs/config_lora_sdxl-base-1.0_ai-char-1.1.toml
